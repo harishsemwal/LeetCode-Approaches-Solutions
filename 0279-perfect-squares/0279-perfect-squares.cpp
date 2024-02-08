109966f2472aa5ed0279a5bd29b1b9e5bc74ca41
HARISH SEMWAL <96759566+Harishsemwal@users.noreply.github.com>
2024-02-08 21:31:31 +0530

    Time: 81 ms (71.50%), Space: 11.2 MB (25.84%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,6 +1,14 @@
+// Hint : sabse pehli number Likhna hai fir uska tree banana hai or check kerna hai ki sab tree ke jitne bhi maximum squre bangi to unka minimum nikalana hai...
+    
+// jaise 12 hai to 12 ko hem : one ka square kerkai 12 baar bhi likh sake hain.
+//     12 ko 3 ka squre kekai or fir 1 ka 3 baar square krkai bhi likh sakte hain....
+    
+// Humko always minimum lene hai....
+//     code i = 1 se isliaa chalaya kyunki khud 12 per check kekai fyda nahi hai n ki value already 12 hai....
+
 class Solution {
 public:
-    int solve(int n, vector<int>& memo) {
+    int solve(int n, vector<int> &memo){
         if (n < 0) {
             return INT_MAX;
         }
@@ -23,3 +31,4 @@ public:
         return solve(n, memo);
     }
 };
+
