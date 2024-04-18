045934cf4cccbfc8f7be7a124faa5e3bef627607
HARISH SEMWAL <96759566+Harishsemwal@users.noreply.github.com>
2024-04-18 10:33:29 +0530

    Time: 84 ms (28.86%), Space: 100.6 MB (94.99%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -4,30 +4,19 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        // vector<int<int>> vt(n+1, vector<int>(m+1, -1));
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < m; j++){
-        //         if(grid[i][j] == 1 || grid[i][j] == 0){
-        //             vt[i][j] = grid[i][j];
-        //         }
-        //     }
-        // }
-        
-        
+        int result = 0;
+
         int rows[4] = {0, 0, -1, 1};
         int cols[4] = {-1, 1, 0, 0};
-        
-        int result = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m ; j++){
-                if(grid[i][j] == 1){
-                    
-                    
-                    for(int k = 0; k < 4; k++){
-                        int nrow = i + rows[k];
-                        int ncol = j + cols[k];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        int ni = i + rows[k];
+                        int nj = j + cols[k];
                         
-                        if(nrow < 0 || nrow >= n || ncol < 0 || ncol >= m || grid[nrow][ncol] == 0){
+                        if (ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] == 0) {
                             result += 1;
                         }
                     }
